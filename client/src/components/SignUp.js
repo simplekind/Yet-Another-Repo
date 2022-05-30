eb34fa71ca50cf8ad0f67a98af76c8bf2781080b
simplekind <harshitgoyal394@gmail.com>
2022-05-31 00:33:43 +0530

    Server

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -24,31 +24,27 @@ function SignUp({ setIsAuth }) {
   return (
     <div className="signUp">
       <label> Sign Up</label>
-      <input
-        placeholder="First Name"
-        onChange={(e) => {
+      
+      <input placeholder="First Name" onChange={(e) => {
           setUser({ ...user, firstName: e.target.value });
         }}
       />
-      <input
-        placeholder="Last Name"
-        onChange={(e) => {
+
+      <input placeholder="Last Name" onChange={(e) => {
           setUser({ ...user, lastName: e.target.value });
         }}
       />
-      <input
-        placeholder="Username"
-        onChange={(e) => {
+      
+      <input placeholder="Username" onChange={(e) => {
           setUser({ ...user, username: e.target.value });
         }}
       />
-      <input
-        placeholder="Password"
-        type="password"
-        onChange={(e) => {
+      
+      <input placeholder="Password" type="password" onChange={(e) => {
           setUser({ ...user, password: e.target.value });
         }}
       />
+      
       <button onClick={signUp}> Sign Up</button>
     </div>
   );
